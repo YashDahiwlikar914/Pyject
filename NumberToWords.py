4740f57bba4285766a1cce4624cdbdfbd6f11462
Yash Dahiwlikar <yashdahiwlikar999@gmail.com>
2024-10-11 21:01:20 +0530

    New File Added!

---
+++ NumberToWords.py
@@ -0,0 +1,7 @@
+Num = input("Number : ")
+
+i = 0
+while i < len(Num):
+    Numbers = {"1": "One", "2": "Two", "3": "Three","4":"Four", "5" : "Five", "6" : "Six", "7": "Seven", "8": "Eight","9": "Nine"}
+    print(Numbers.get(Num[i]), end=" ")
+    i = i + 1
